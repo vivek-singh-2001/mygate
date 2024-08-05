269f355c4a5402bfd6f87daf023e52b516f044ce
vivek-singh-2001 <kaal4Study@gmail.com>
2024-08-05 15:10:42 +0530

    added update vendor

--- backend/routes/user_route.js
+++ backend/routes/user_route.js
@@ -3,9 +3,8 @@ const user_controller = require("../controller/user_controller");
 
 const router = express.Router();
 
-router.route("/").get(user_controller.getAllUser);
+router.route("/:id")
+    .get(user_controller.getUserById)
+    .post(user_controller.updateUser)
 
-router.route("/:societyId").get(user_controller.getUsersBySociety);
-
-router.route("/:societyId/:wingName").get(user_controller.getUsersBySocietyAndWing);
 module.exports = router;
