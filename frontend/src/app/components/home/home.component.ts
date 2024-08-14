b1837aab705f1bf159d5d1fac24c561b7f493317
vivek-singh-2001 <kaal4Study@gmail.com>
2024-08-14 10:53:48 +0530

    some changes

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -1,17 +1,24 @@
-import { Component } from '@angular/core';
-import { AuthService } from '../../services/auth.service';
+import { Component, ViewChild } from '@angular/core';
+import { SidebarModule } from 'primeng/sidebar';
+import { ButtonModule } from 'primeng/button';
+import { RippleModule } from 'primeng/ripple';
+import { AvatarModule } from 'primeng/avatar';
+import { StyleClassModule } from 'primeng/styleclass';
+import { Sidebar } from 'primeng/sidebar';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+    selector: 'sidebar-headless-demo',
+    templateUrl: './home.component.html',
+    standalone: true,
+    imports: [SidebarModule, ButtonModule, RippleModule, AvatarModule, StyleClassModule,CommonModule]
 })
 export class HomeComponent {
+    @ViewChild('sidebarRef') sidebarRef!: Sidebar;
 
-  constructor(private authService: AuthService){}
-  onLogout(){
-    this.authService.logout();
-  }
+    closeCallback(e:any): void {
+        this.sidebarRef.close(e);
+    }
+
+    sidebarVisible: boolean = false;
 }
