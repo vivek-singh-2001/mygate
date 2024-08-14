8294bd7751873d946a3a45fef37f61e620a76045
vivek-singh-2001 <kaal4Study@gmail.com>
2024-08-14 17:56:34 +0530

    dashboard

---
+++ frontend/src/app/components/home/main-display/main-display.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-display',
+  standalone: true,
+  imports: [],
+  templateUrl: './main-display.component.html',
+  styleUrl: './main-display.component.css'
+})
+export class MainDisplayComponent {
+
+}
