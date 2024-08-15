afb1de5749852fc21a3cbb5d187e177ff4ff5746
vivek <kaal4study@gmail.com>
2024-08-15 20:18:30 +0530

    nav

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { NavigationComponent } from "./navigation/navigation.component";
 import { SideNavComponent } from "./side-nav/side-nav.component";
 import { MainDisplayComponent } from "./main-display/main-display.component";
@@ -7,11 +8,19 @@ import { MainDisplayComponent } from "./main-display/main-display.component";
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavigationComponent, SideNavComponent, MainDisplayComponent],
+  imports: [
+    NavigationComponent,
+    SideNavComponent,
+    MainDisplayComponent,
+    CommonModule,
+  ],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
+  isSidebarExpanded: boolean = false;
 
-  
+  onSidebarExpansionChanged(expanded: boolean) {
+    this.isSidebarExpanded = expanded;
+  }
 }
--- frontend/src/app/components/home/side-nav/side-nav.component.ts
+++ frontend/src/app/components/home/side-nav/side-nav.component.ts
@@ -1,15 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, EventEmitter,Output } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { SidebarModule } from 'primeng/sidebar';
+import { CommonModule } from '@angular/common';
+import { MenubarModule } from 'primeng/menubar';
+import { MenuItem } from 'primeng/api';
+import { MenuModule } from 'primeng/menu';
 
 
 @Component({
   selector: 'app-side-nav',
   standalone: true,
-  imports: [SidebarModule,ButtonModule],
+  imports: [
+    SidebarModule,
+    ButtonModule,
+    CommonModule,
+    MenubarModule,
+    MenuModule,
+  ],
   templateUrl: './side-nav.component.html',
-  styleUrl: './side-nav.component.css'
+  styleUrl: './side-nav.component.css',
 })
-export class SideNavComponent {
-  sidebarVisible: boolean = false;
+export class SideNavComponent implements OnInit {
+  @Output() expansionChanged = new EventEmitter<boolean>();
+
+  isExpanded: boolean = false;
+  items!: MenuItem[];
+
+  ngOnInit() {
+    this.items = [
+      { label: 'Dashboard', icon: 'pi pi-home' },
+      { label: 'Profile', icon: 'pi pi-user' },
+      { label: 'Settings', icon: 'pi pi-cog' },
+      { label: 'Messages', icon: 'pi pi-envelope' },
+    ];
+  }
+
+  toggleSidebar() {
+    this.isExpanded = !this.isExpanded;
+    this.expansionChanged.emit(this.isExpanded);
+  }
 }
