7c09d30eb40cc71fcd16b30ec10f1fc38530abaa
vivek-singh-2001 <kaal4Study@gmail.com>
2024-08-31 15:42:33 +0530

    working on profile

--- frontend/src/app/components/user-detail/user-detail.component.ts
+++ frontend/src/app/components/user-detail/user-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 import { AvatarModule } from 'primeng/avatar';
 import { ButtonModule } from 'primeng/button';
 import { TabViewModule } from 'primeng/tabview';
@@ -7,30 +7,46 @@ import { DividerModule } from 'primeng/divider';
 import { CalendarModule } from 'primeng/calendar';
 import { DropdownModule } from 'primeng/dropdown';
 import { InputTextModule } from 'primeng/inputtext';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { InputMaskModule } from 'primeng/inputmask';
+import { AutoCompleteModule } from 'primeng/autocomplete';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+
+
+interface Gender {
+  label: string;
+  value: string;
+}
 
 @Component({
   selector: 'app-user-detail',
   standalone: true,
-  imports: [  TabViewModule,AvatarModule,ButtonModule,ImageModule,DividerModule,CalendarModule,DropdownModule,InputTextModule],
+  imports: [
+    TabViewModule,
+    AvatarModule,
+    ButtonModule,
+    InputMaskModule,
+    InputNumberModule,
+    ImageModule,
+    DividerModule,
+    CalendarModule,
+    DropdownModule,
+    InputTextModule,
+    AutoCompleteModule,
+    InputTextareaModule
+  ],
   templateUrl: './user-detail.component.html',
   styleUrl: './user-detail.component.css',
 })
-export class UserDetailComponent {
+export class UserDetailComponent  {
+  today: Date = new Date();
+  genders: Gender[] = [];
 
-  bloodGroups = [
-    { name: 'A+', value: 'A+' },
-    { name: 'A-', value: 'A-' },
-    { name: 'B+', value: 'B+' },
-    { name: 'B-', value: 'B-' },
-    { name: 'AB+', value: 'AB+' },
-    { name: 'AB-', value: 'AB-' },
-    { name: 'O+', value: 'O+' },
-    { name: 'O-', value: 'O-' }
+  constructor(){
+  this.genders = [
+    { label: 'Male', value: 'male' },
+    { label: 'Female', value: 'female' },
+    { label: 'Other', value: 'other' },
   ];
-
-  genders = [
-    { name: 'Male', value: 'male' },
-    { name: 'Female', value: 'female' },
-    { name: 'Other', value: 'other' }
-  ];
+}
 }
