64cf843098120eab214dd986c9d44baeb12b9c67
dhruvil2533 <dhruvilmodi2503@gmail.com>
2024-09-27 10:17:34 +0530

    event changes

--- backend/features/events/eventService.js
+++ backend/features/events/eventService.js
@@ -1,29 +1,24 @@
-const eventRepository = require('./eventRepository');
-const CustomError = require('../../utils/CustomError');
- 
-const eventService = {
-  getAllEvents: async () => {
-    return await eventRepository.getAllEvents();
-  },
+const eventRepository = require("./eventRepository");
+const CustomError = require("../../utils/CustomError");
 
-  createEvent: async (eventData) => {
-  
-    return await eventRepository.createEvent(eventData);
-  },
-
-  updateEvent: async (id, eventData, userRole) => {
-    if (userRole !== 'admin') {
-      throw new CustomError('Only admins can update events', 403);
-    }
-    return await eventRepository.updateEvent(id, eventData);
-  },
-
-  deleteEvent: async (id, userRole) => {
-    if (userRole !== 'admin') {
-      throw new CustomError('Only admins can delete events', 403);
-    }
-    return await eventRepository.deleteEvent(id);
-  },
+exports.getAllEvents = async (eventId) => {
+  return await eventRepository.getAllEvents(eventId);
 };
 
-module.exports = eventService;
+exports.createEvent = async (eventData) => {
+  return await eventRepository.createEvent(eventData);
+};
+
+exports.updateEvent = async (id, eventData, userRole) => {
+  if (userRole !== "admin") {
+    throw new CustomError("Only admins can update events", 403);
+  }
+  return await eventRepository.updateEvent(id, eventData);
+};
+
+exports.deleteEvent = async (id, userRole) => {
+  if (userRole !== "admin") {
+    throw new CustomError("Only admins can delete events", 403);
+  }
+  return await eventRepository.deleteEvent(id);
+};
