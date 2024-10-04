b37b7d9f4381d849aa1158a5baa9e28bab2e8cab
dhruvil2533 <dhruvilmodi2503@gmail.com>
2024-10-04 10:40:27 +0530

    migration change

--- backend/migrations/20240802041455-create_get_users_by_society_procedure.js
+++ backend/migrations/20240802041455-create_get_users_by_society_procedure.js
@@ -11,12 +11,7 @@ module.exports = {
 
     // Create procedure to fetch users by society ID
     await queryInterface.sequelize.query(`
-      CREATE OR REPLACE FUNCTION GetUsersBySociety(
-        society_id UUID, 
-        limits INT DEFAULT 10, 
-        offsets INT DEFAULT 0, 
-        searchQuery VARCHAR DEFAULT NULL
-      )
+      CREATE OR REPLACE FUNCTION GetUsersBySociety(society_id INT, limits INT DEFAULT 10, offsets INT DEFAULT 0, searchQuery VARCHAR DEFAULT NULL)
       RETURNS TABLE(
         id UUID,
         firstname VARCHAR,
@@ -24,34 +19,28 @@ module.exports = {
         email VARCHAR,
         photo VARCHAR,
         number VARCHAR,
-        house_no VARCHAR,
-        total_count INT  -- Add total_count to the return structure
+        house_no INT,
+        total_count INT
       )
       LANGUAGE plpgsql
       AS $$
       BEGIN
         RETURN QUERY
         SELECT 
-          u.id, 
-          u.firstname, 
-          u.lastname, 
-          u.email, 
-          u.photo, 
-          u.number, 
-          h.house_no,
-          (SELECT COUNT(*)::int 
-           FROM "users" u
-           JOIN "houseUsers" hu ON u.id = hu."userId"
-           JOIN "houses" h ON hu."houseId" = h.id
-           JOIN "wings" w ON h."wingId" = w.id
-           WHERE w."SocietyId" = society_id 
-             AND (
+          u.id, u.firstname, u.lastname, u.email, u.photo, u.number, h.house_no,
+          (SELECT COUNT(*)::int FROM "users" u
+            JOIN "houseUsers" hu ON u.id = hu."userId"
+            JOIN "houses" h ON hu."houseId" = h.id
+            JOIN "floors" f ON h."floorId" = f.id
+            JOIN "wings" w ON f."wingId" = w.id
+            WHERE w."societyId" = society_id 
+            AND (
               searchQuery IS NULL OR searchQuery = '' OR
               u.firstname ILIKE '%' || searchQuery || '%' OR
               u.lastname ILIKE '%' || searchQuery || '%' OR
               u.email ILIKE '%' || searchQuery || '%' OR
               u.number LIKE '%' || searchQuery || '%'
-             )
+            )
           ) AS total_count  
         FROM 
           "users" u
@@ -60,9 +49,11 @@ module.exports = {
         JOIN 
           "houses" h ON hu."houseId" = h.id
         JOIN 
-          "wings" w ON h."wingId" = w.id
+          "floors" f ON h."floorId" = f.id
+        JOIN 
+          "wings" w ON f."wingId" = w.id
         WHERE 
-          w."SocietyId" = society_id
+          w."societyId" = society_id
           AND (
             searchQuery IS NULL OR searchQuery = '' OR
             u.firstname ILIKE '%' || searchQuery || '%' OR
@@ -77,28 +68,21 @@ module.exports = {
 
     // Create procedure to fetch users by society ID and wing name
     await queryInterface.sequelize.query(`
-      CREATE OR REPLACE FUNCTION GetUsersBySocietyAndWing(
-        society_id UUID, 
-        wing_id UUID
-      )
+      CREATE OR REPLACE FUNCTION GetUsersBySocietyAndWing(society_id UUID, wing_id UUID)
       RETURNS TABLE(
         id UUID,
         firstname VARCHAR,
         lastname VARCHAR,
         email VARCHAR,
         number VARCHAR,
-        house_no VARCHAR
+        house_no INT
       )
       LANGUAGE plpgsql
       AS $$
       BEGIN
         RETURN QUERY
         SELECT 
-          u.id, 
-          u.firstname, 
-          u.lastname, 
-          u.email, 
-          u.number,
+          u.id, u.firstname, u.lastname, u.email, u.number,
           h.house_no
         FROM 
           "users" u
@@ -107,10 +91,12 @@ module.exports = {
         JOIN 
           "houses" h ON hu."houseId" = h.id
         JOIN 
-          "wings" w ON h."wingId" = w.id
+          "floors" f ON h."floorId" = f.id
+        JOIN 
+          "wings" w ON f."wingId" = w.id
         WHERE 
-          w."SocietyId" = society_id 
-          AND w.id = wing_id;
+          w."societyId" = society_id AND
+          w.id = wing_id;
       END;
       $$;
     `);
