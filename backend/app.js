b937ac403748456c02c69dcaf3de943d9643cd58
dhruvil2533 <dhruvilmodi2503@gmail.com>
2024-10-09 15:28:50 +0530

    all corrected

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,4 @@
+
 const express = require("express");
 const morgan = require("morgan");
 const CustomError = require("./utils/CustomError");
@@ -7,7 +8,6 @@ const cors = require("cors");
 const session = require("express-session");
 const passport = require("./config/passport");
 const path = require("path");
-
 // import routes here
 const user_route = require("./features/users/userApis");
 const society_route = require("./features/society/societyApis");
@@ -18,14 +18,25 @@ const auth_route = require("./features/authentication/authApi");
 const sms_route = require("./features/sms/smsApis");
 const event_route = require("./features/events/eventApi");
 const chatRoutes = require("./features/chat/chatApi");
-
 // USE MODULES HERE
 const app = express();
-
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
-
+const allowedOrigins = [
+  'http://localhost:4200',
+  'http://192.1.200.38:4200'
+];
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+}));
 // Serve static files from the 'dist' directory
 app.use(
   "/static",
@@ -40,16 +51,8 @@ app.use(
     },
   })
 );
-
-// Route all other requests to Angular
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/frontend/browser/index.html'));
-});
-
-app.use(cors({ origin: "http://192.1.200.38:4200", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
-
 // Session configuration
 app.use(
   session({
@@ -63,10 +66,8 @@ app.use(
     },
   })
 );
-
 app.use(passport.initialize());
 app.use(passport.session());
-
 // USE ROUTES HERE
 app.use("/api/v1/users", user_route);
 app.use("/api/v1/society", society_route);
@@ -84,8 +85,10 @@ app.use("*", (req, res, next) => {
   );
   next(err);
 });
-
+// Route all other requests to Angular
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/frontend/browser/index.html'));
+});
 // ERROR HANDLER MUST BE DEFINED LAST
 app.use(globalErrorHandler);
-
 module.exports = app;
