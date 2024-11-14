3aadcad4d66ffabf026c753b12f82fd5f2b8c3ce
vivek-singh-2001 <kaal4study@gmail.com>
2024-11-14 11:31:12 +0530

    Remove environment files from tracking

--- frontend/src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'http://192.1.200.38:7500/api/v1',
-  enableDebug: false
-};
--- frontend/src/environments/environment.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: false,
-  // apiUrl: 'http://192.1.200.38:7500/api/v1',
-  // socketUrl: 'http://192.1.200.38:7500',
-  apiUrl: 'http://localhost:7500/api/v1',
-  socketUrl: 'http://localhost:7500',
-  googleMapsApiKey: 'AIzaSyC4H0rmF3aKDg7CKmO6wZFOriMMJEXsH3M',
-  enableDebug: true
-};
